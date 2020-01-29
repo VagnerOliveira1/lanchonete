8fa743fdf7fe4f6544924b909b98d0ef63969d80
Vagner Oliveira <ti.vagner@gmail.com>
2020-01-29 05:55:37 -0300

    Inclui total itens no carrinho

--- app/helpers/administradors_helper.rb
+++ app/helpers/administradors_helper.rb
@@ -1,8 +1,3 @@
 module AdministradorsHelper
-  def existe_carrinho?(id)
-    return false if cookies[:carrinho].blank?
-    produtos = JSON.parse(cookies[:carrinho]);
-    produtos.include?(id.to_s)
-  end
-
+  
 end
